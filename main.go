69c18ae7e41199d42dcd4f1aebfd835f8689de8e
Akhilesh Singh <akhilesh.singh@myntra.com>
2017-08-24 15:36:21 +0530

    changes

--- main.go
+++ main.go
@@ -14,12 +14,12 @@ func main() {
 		status.Status()
 	} else if flags.Action == "statsd" {
 		statsd.Statsd()
-	} else if flags.Action == "create-cluster" {
+	} else if flags.Action == "cluster" {
 		cluster.ClusterCreate()
 	} else {
-		fmt.Println("Usage: ./smartRedis -action=[status|statsd|cluster]")
+		fmt.Println("Usage: ./smartRedis -action=[status|statsd|cluster] -redisHost=localhost -redisPort=30001 -resolveHostname=n")
 		fmt.Println("status          -- Show Cluster status.")
-		fmt.Println("create-cluster  -- Launch Redis Cluster instances.")
+		fmt.Println("cluster  -- Launch Redis Cluster instances.")
 		fmt.Println("statsd          -- Push metrics to statsd.")
 	}
 }
